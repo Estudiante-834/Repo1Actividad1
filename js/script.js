0c5d233887225779e83f94861e249f5b13396c89
Estudiante-834 <Estudiante_834@outlook.com>
2024-11-13 04:46:11 +0100

    ADD: Implementado funcionalidad básica de borrado de <li>s existentes en <ul>

--- js/script.js
+++ js/script.js
@@ -4,11 +4,18 @@ function addMessage() {
 
     newMessage.innerText = inputMessage.value;
     messageList.appendChild(newMessage);
-
     inputMessage.value = "";
 }
 
+function resetMessage() {
+    let messageList = document.getElementById("mssg-list");
+    
+    messageList.innerHTML = "";
+}
+
 let inputMessage = document.getElementById("input-mssg");
-let addButton = document.getElementById("add-button")
+let addButton = document.getElementById("add-button");
+let resetButton = document.getElementById("reset-button");
 
 addButton.addEventListener("click", addMessage);
+resetButton.addEventListener("click", resetMessage);
